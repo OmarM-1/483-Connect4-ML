45642d33b94ae6511c75b530df7670e447f54902
Nathaniel Obeso <nathaniel.obeso@csu.fullerton.edu>
2026-04-13 18:41:15 -0700

    Ensured same result for first improved model

--- src/train_improved1.py
+++ src/train_improved1.py
@@ -4,7 +4,7 @@ from dataset import load_train_val
 
 def main():
     X_train, y_train, X_val, y_val = load_train_val()
-    model = RandomForestClassifier(criterion = 'entropy', max_depth=6)
+    model = RandomForestClassifier(criterion = 'entropy', max_depth=6, random_state = 1)
 
     model.fit(X_train, y_train)
 
